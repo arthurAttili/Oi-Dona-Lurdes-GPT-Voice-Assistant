a3176d9cb4cf84caf60c8b5bc290cb61361371c9
Arthur Attili <38759435+arthurAttili@users.noreply.github.com>
2024-01-24 19:45:24 -0300

    Create configs.py

---
+++ configs.py
@@ -0,0 +1,15 @@
+WAKE_WORD = "Oi Dona Lurdes"
+IDIOMA = 'pt-BR'
+LIMITE_TEMPO_DUVIDA = 5 #Tempo limite para falar a dúvida ao GPT. Em segundos.
+SAUDACOES = [f"Ola, meu querido!",
+             "Oi! Estou aqui",
+             "Como vai a vida, anjo?"]
+
+OPENAI_API_KEY = 'KEY'
+#MODELO_GPT = 'gpt-4-1106-preview' #-> Para maior precisão
+MODELO_GPT = 'gpt-3.5-turbo' #-> Para maior velocidade
+
+PERSONALIDADE_GPT = f"""Você é minha assistente pessoal. 
+    Responda a qualquer pergunta que eu fizer como uma senhora paulistana de 70 anos responderia. 
+    Use no máximo 100 palavras.
+    """
